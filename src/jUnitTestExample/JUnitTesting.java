193544e77d664ff9f234fbfdc61b76f1ab8d2e13
kanikamittal <kanikamittal1811@users.noreply.github.com>
2020-05-11 02:44:58 +0530

    added functions

---
+++ src/jUnitTestExample/JUnitTesting.java
@@ -0,0 +1,19 @@
+package jUnitTestExample;
+
+
+public class JUnitTesting {
+	
+	public static int addNumbers(int a , int b) {
+		return a+b;
+	}
+	public static String addStrings(String a, String b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(addStrings("Hello"," World"));
+		System.out.println(addNumbers(2019,1));
+	}
+
+}
