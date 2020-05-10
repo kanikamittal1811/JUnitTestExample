4a4f88415706c2c426d78cb75e912d1a3806328a
kanikamittal <kanikamittal1811@users.noreply.github.com>
2020-05-11 02:56:45 +0530

    c

--- src/jUnitTestExample/JUnitTesting.java
+++ src/jUnitTestExample/JUnitTesting.java
@@ -6,7 +6,9 @@ public class JUnitTesting {
 	public static int addNumbers(int a , int b) {
 		return a+b;
 	}
-	
+	public static String addStrings(String a, String b) {
+		return a+b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
